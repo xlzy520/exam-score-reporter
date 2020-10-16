a576e305464a3c40dd644fa98095988ba3d6b4bc
xlzy <1017866168@qq.com>
2020-10-16 14:50:58 +0800

    add: login云函数

--- cloudfunctions/login/index.js
+++ cloudfunctions/login/index.js
@@ -6,14 +6,14 @@ const cloud = require('wx-server-sdk')
 // 初始化 cloud
 cloud.init({
   // API 调用都保持和云函数当前所在环境一致
-  env: cloud.DYNAMIC_CURRENT_ENV
+  env: cloud.DYNAMIC_CURRENT_ENV,
 })
 
 /**
  * 这个示例将经自动鉴权过的小程序用户 openid 返回给小程序端
- * 
+ *
  * event 参数包含小程序端调用传入的 data
- * 
+ *
  */
 exports.main = async (event, context) => {
   console.log(event)
@@ -33,4 +33,3 @@ exports.main = async (event, context) => {
     env: wxContext.ENV,
   }
 }
-
