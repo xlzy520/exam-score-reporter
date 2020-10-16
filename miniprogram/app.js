de642720b6a0583ea629201d0f33774264536448
xlzy <1017866168@qq.com>
2020-10-16 14:52:14 +0800

    format: 格式化

--- miniprogram/app.js
+++ miniprogram/app.js
@@ -1,6 +1,6 @@
-//app.js
+// app.js
 App({
-  onLaunch: function () {
+  onLaunch() {
     
     if (!wx.cloud) {
       console.error('请使用 2.2.3 或以上的基础库以使用云能力')
@@ -16,5 +16,5 @@ App({
     }
 
     this.globalData = {}
-  }
+  },
 })
