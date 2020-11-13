edaad454670dc0b973c2f2302f4d00b6df2ea9cf
xlzy <1017866168@qq.com>
2020-11-13 12:00:44 +0800

    update: 优化年级学期选择为学段-年级-学期选择

--- components/gradePicker/index.vue
+++ components/gradePicker/index.vue
@@ -3,19 +3,22 @@
     <van-cell :title="title" is-link :icon="icon" :value="currentGrade"
               arrow-direction="down" @click="showPopup"></van-cell>
     <van-popup :show="show" position="bottom" @close="onClose">
-      <van-picker :columns="columns" show-toolbar @cancel="onClose" @confirm="onConfirm" />
+      <van-picker id="picker" :columns="originColumns" @change="change" show-toolbar
+                  @cancel="onClose" @confirm="onConfirm" />
     </van-popup>
   </view>
 </template>
 
 <script>
-import { gradeAndTermColumn } from '../../utils/enum'
+import {
+  gradeAndTermColumn, gradeColumn, levelColumn, levelGradeEnum, levelIndex
+} from '../../utils/enum'
 
 export default {
   data() {
     return {
       show: false,
-      columns: gradeAndTermColumn,
+      originColumns: gradeAndTermColumn,
     }
   },
 
@@ -48,7 +51,33 @@ export default {
   },
 
   methods: {
+    change(evt) {
+      const { picker, value, index } = evt.detail
+      picker.setColumnValues(1, levelGradeEnum[value[0]])
+    },
+    updateColumns() {
+      const picker = this.selectComponent('#picker')
+      const columns = JSON.parse(JSON.stringify(this.originColumns))
+      const gradeIndexInAll = gradeColumn.findIndex(f => f === this.grade)
+      const _levelIndex = levelIndex.findIndex(f => gradeIndexInAll <= f)
+      picker.setColumnIndex(0, _levelIndex)
+      const currentGradeArr = levelGradeEnum[levelColumn[_levelIndex]]
+      const grade = columns[1].values
+      const term = columns[2].values
+      const gradeIndex = currentGradeArr.findIndex(f => f === this.grade)
+      const termIndex = term.findIndex(f => f === this.term)
+      console.log(gradeIndexInAll, gradeIndex)
+
+      picker.setColumnIndex(1, gradeIndex)
+      picker.setColumnIndex(2, termIndex)
+      // columns[0].defaultIndex = _levelIndex
+      // columns[1].defaultIndex = gradeIndex
+      // columns[1].values = levelGradeEnum[levelColumn[_levelIndex]]
+      // columns[0].defaultIndex = grade.findIndex(f => f === this.grade)
+      // this.originColumns = columns
+    },
     showPopup() {
+      this.updateColumns()
       this.show = true
     },
 
@@ -60,6 +89,8 @@ export default {
       const {
         value,
       } = event.detail
+      console.log(value)
+      value.shift()
       this.$emit('change', { detail: value })
       this.onClose()
     },
