9c6e984e761c2fe4e1e603706db6f66d1046b260
xlzy <1017866168@qq.com>
2020-11-13 16:18:04 +0800

    update: 优化反馈组件

--- components/feedback/index.vue
+++ components/feedback/index.vue
@@ -9,10 +9,14 @@
         <van-field class="feedback" :value="content" @change="changeValue"
                    type="textarea"  :autosize="{ minHeight: 50 }" auto-focus
                    placeholder="请输入问题反馈或需求提交" />
-        <van-radio-group :value="issueType" @change="changeIssueType">
-          <van-radio name="1">单选框 1</van-radio>
-          <van-radio name="2">单选框 2</van-radio>
-        </van-radio-group>
+        <view class="issue-type">
+          <view class="issue-type-title">问题类型： </view>
+          <van-radio-group :value="issueType" @change="changeIssueType"
+                           class="issue-type-radios">
+            <van-radio name="bug">BUG</van-radio>
+            <van-radio name="req">需求</van-radio>
+          </van-radio-group>
+        </view>
         <van-button class="padding-btn" block round @tap="submit" type="primary">
           提交
         </van-button>
@@ -31,8 +35,8 @@ export default {
   data() {
     return {
       show: false,
-      content: '',
-      issueType: ''
+      content: ' ',
+      issueType: 'bug',
     }
   },
 
@@ -54,6 +58,9 @@ export default {
   methods: {
     showPopup() {
       this.show = true
+      setTimeout(() => {
+        this.content = ''
+      }, 300)
     },
 
     onClose() {
@@ -64,6 +71,7 @@ export default {
       this.content = evt.detail
     },
     changeIssueType(evt) {
+      console.log(evt)
       this.issueType = evt.detail
     },
     submit() {
@@ -74,13 +82,16 @@ export default {
       uni.showLoading({
         title: '提交中...',
       })
+      const systemInfo = wx.getSystemInfoSync()
       api.wxCloudCallFunction('add', {
         collectionName: 'report',
         content: this.content,
+        issueType: this.issueType,
+        systemInfo,
       }).then(res => {
         Toast('添加成功')
         console.log(res)
-        // this.$emit('change', event.detail)
+        this.$emit('change', event.detail)
         this.onClose()
       }).finally(() => {
         uni.hideLoading()
@@ -93,5 +104,23 @@ export default {
 <style lang="scss">
   .feedback{
     min-height: 200upx;
+  }
+  .issue-type{
+    display: flex;
+    align-items: center;
+    padding: 0 30upx;
+    &-title{
+      font-size: 36upx;
+      line-height: 50upx;
+      color: #333;
+      margin-right: 30upx;
+    }
+  }
+  .issue-type-radios{
+    display: flex;
+    justify-content: space-around;
+    height: 120upx;
+    align-items: center;
+    flex-grow: 1;
   }
 </style>
